ae703a2379e0e24d485e70e6800f346edb9d555b
Yogi Satharla <yogisatharla@gmail.com>
2025-07-05 04:43:50 +0000

    changes i have made in the app are not going live

---
+++ src/next.config.ts
@@ -0,0 +1,35 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "https://6000-firebase-studio-1747286838448.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev",
+    ],
+  },
+};
+
+export default nextConfig;
